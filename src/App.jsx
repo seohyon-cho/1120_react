20510070240ee4a66a8f38920783c6b1382c1537
seohyon-cho <flanique@naver.com>
2023-11-20 10:58:54 +0900

    컴포넌트에 대한 설명 (필수 조건, JSX의 개념)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 function App() {
 	return (
 		<div className='App'>
-			<h1>World</h1>
+			<h2>World</h2>
 		</div>
 	);
 }
 
 export default App;
+
+/*
+	컴포넌트 함수의 필수 조건
+	1. 함수명은 대문자로 시작되어야 함. 
+	2. JSX를 리턴해야 함. 
+
+	// JSX : 리액트에서 전용으로 쓰이는 DOM 생성 문법
+	원래는 React.createElement() 라는 메서드를 이용해서 복잡하게 구성해야 했던 로직을, HTML 태그를 따라한 친화적인 문법을 이용해 편하게 DOM을 생성하기 위한 리액트만의 문법체계.
+*/
