ac206b1c8ddf8866fc25145b6813be720e362449
seohyon-cho <flanique@naver.com>
2023-11-21 10:53:36 +0900

    리액트에서 일반 변수로 값을 담으면 안 되는 이유

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 export default function App() {
-	// 리액트에서는 특정 DOM(가상DOM) 요소 선택 시, 가급적 document.querySelector 사용은 지양할 것.
-	// 리액트 구조상 document.querySelector는 현재 작업하고 있는 최신 가상DOM이 아닌, 이미 한 번 real DOM 형태로 변환된 예전 DOM을 찾게 되므로 DOM요소의 신뢰성이 떨어짐.
+	console.log('re-render');
+	// 해당 경우에는 이벤트 발생 시 state의 정보값을 변경하는 것이 아닌
+	// 일반 변수의 값을 변경하고 있기 때문에
+	// react 입장에서는, 해당 내용의 변경 사항을 중요하게 인식하지 않아서 컴포넌트를 재호출(re-render)하지 않음.
+	let num = 0;
+	// 위와 같이 직접 변수값을 변경해도, 화면에서의 변경은 일어나지만, 이 변경사항은 react의 제어에서 벗어난, 일반적인 DOM의 구조가 변경되는 것뿐임.
+	// 굳이 번거롭게 일반 변수가 아닌 state라는 것만 가지고 화면을 제어하도록 강제하는 이유는?
+
 	const changeButton = (e, txt) => {
-		e.target.innerText = txt;
+		e.target.innerText = txt + num++;
 	};
 
 	return (
 		<>
 			<section>
-				<button onClick={(e) => changeButton(e, '버튼2')}>버튼</button>
+				<button onClick={(e) => changeButton(e, 'button')}>버튼</button>
 			</section>
 		</>
 	);
 }
+
+/*
+	리액트에서의 re-render, re-paint 개념
+	re-render : 해당 컴포넌트를 재호출
+
+	리액트에서 re-render를 발생하는 경우는 다음과 같다. 
+	1. 물리적으로 JSX의 구조가 변경되는 경우
+	2. 무조건 state라는 정보값이 변경되는 경우
+*/
