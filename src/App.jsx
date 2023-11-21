39f4e726641b5244350ab9c8973b2e209b3564fd
seohyon-cho <flanique@naver.com>
2023-11-21 11:51:37 +0900

    map으로 특정 데이터를 반복 돌면서 동적으로 JSX 반환 시 key 값이 필요한 이유

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Box from './box/Box';
-
 export default function App() {
-	// 박스 클릭 시, 인수로 원하는 색상값을 문자열로 집어넣어서 해당 박스의 배경 색 변경하는 핸들러 함수 따로 만든 뒤, 클릭 이벤트 시 호출.
+	const colors = ['orange', 'hotpink', 'aqua', 'green', 'orangered', 'cornflowerblue'];
+
 	return (
 		<>
-			<Box />
+			<h1>Color Chart</h1>
+
+			<ul>
+				{colors.map((color, idx) => {
+					// return문에서 map을 통해 반복적으로 생성되는 가상DOM 요소에는, 무조건 고유의 key값을 부여해줘야 함.
+					// 이유 : react로 하여금 여러 개의 반복 요소 중에서 수정해야 하는 요소를 효율적으로 비교탐색하기 위해서.
+					// 간편하게는 반복되는 순서값(ids)만을 집어넣을 수도 있긴 하나, 권장하지는 않으므로
+					// DB의 각 데이터에 내장되어 있는 Object_id를 활용하거나, 실제 데이터와 순번을 결합 (color+idx) 해서 고유 문자 값을 생성하는 것이 바람직함.
+					return (
+						<li key={color + idx} style={{ color: color }}>
+							{color}
+						</li>
+					);
+				})}
+			</ul>
 		</>
 	);
 }
