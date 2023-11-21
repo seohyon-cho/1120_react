eee8c6d3e5343c699130bceecc5ec67e4a3082a2
seohyon-cho <flanique@naver.com>
2023-11-21 10:35:28 +0900

    리액트에서는 querySelector 사용 지양에 대한 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 export default function App() {
+	// 리액트에서는 특정 DOM(가상DOM) 요소 선택 시, 가급적 document.querySelector 사용은 지양할 것.
+	// 리액트 구조상 document.querySelector는 현재 작업하고 있는 최신 가상DOM이 아닌, 이미 한 번 real DOM 형태로 변환된 예전 DOM을 찾게 되므로 DOM요소의 신뢰성이 떨어짐.
 	const changeButton = (e, txt) => {
 		e.target.innerText = txt;
 	};
