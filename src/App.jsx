9177d6a55255f524bfc56b0b210c18f4089236d4
seohyon-cho <flanique@naver.com>
2023-11-21 12:47:48 +0900

    props 활용 예제 결과물 (복습 요함)

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,18 @@ export default function App() {
 			<h1>Color Chart</h1>
 			{colors.map((color, idx) => {
 				// 컴포넌트 호출 시, 컴포넌트의 특정 값을 key={value} 형태로 컴포넌트 내부에 전달 가능. (=props 라고 함.)
-				return <Card key={color + idx} colorName={color} index={idx} />;
+				return (
+					<Card
+						key={color + idx}
+						colorName={color}
+						index={idx}
+						width={100 * (idx + 1) + 'px'}
+						show={'block'}
+					/>
+				);
 			})}
+
+			<Card colorName={'black'} index={4} width={600} />
 		</>
 	);
 }
--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -3,10 +3,11 @@ import './Card.scss';
 export default function Card(props) {
 	// 부모 컴포넌트에 전달되는 요소들을 props 라는 객체로 묶어서 자식 컴포넌트의 파라미터로 전달받음.
 	console.log(props);
+	// props 안에는 {colorName: '색상', index: 순서값, width: 넓이값, show: display속성값} 의 구조로 있는 것이므로, props.width, props.colorName 이런식으로 불러와서 쓰는 것임.
 	return (
-		<article className='Card'>
+		<article className='Card' style={{ width: props.width }}>
 			<div className='bgBox' style={{ backgroundColor: props.colorName }}></div>
-			<div className='txtBox'>
+			<div className='txtBox' style={{ display: props.show }}>
 				{props.index + 1} - {props.colorName}
 			</div>
 		</article>
