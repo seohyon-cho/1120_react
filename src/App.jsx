abcb763fabc3c5f40aa34ee84772b77458fa0e8c
seohyon-cho <flanique@naver.com>
2023-11-22 13:18:11 +0900

    state 관련 주의사항 2가지

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react';
 import './App.scss';
 
+/*
+	state 관련 주의 사항
+	1. state 값 변경 시, 해당 값은 무조건 다음 렌더링 사이클에 반영됨. 
+	2. (1)의 특성에 기인해서, 특정 숫자값을 증감시킬 때에는 후위증감연산자를 사용하면 안 됨. 
+*/
+
 export default function App() {
-	// state값 변경 순서에 따라 결과 화면이 달라지는 경우
 	console.log('render');
-	let [Index, setIndex] = useState(0);
+	const [Index, setIndex] = useState(0);
 
 	return (
 		<>
-			{/* setIndex가 실행되는 순간에 0 (해당 값의 초기값) 이 먼저 활용됨. 실제로 setIndex(0), 즉 초기값부터 호출되는 것. */}
-			<button onClick={() => setIndex(Index--)}>left</button>
-			{/* setIndex가 실행되는 순간에 바로 초기값을 먼저 증가시킨 뒤 호출되므로 실제로 setIndex(1)부터 바로 호출됨. */}
-			<button onClick={() => setIndex(++Index)}>right</button>
+			<button onClick={() => setIndex(Index - 1)}>left</button>
+			<button onClick={() => setIndex(Index + 1)}>right</button>
 			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
 		</>
 	);
