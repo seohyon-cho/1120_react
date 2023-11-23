aa3f141dd41f34979a158105a9cf837b1b9bce31
seohyon-cho <flanique@naver.com>
2023-11-23 09:38:35 +0900

    기본 모달 컴포넌트 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from 'react';
-import './App.scss';
-
-/*
-	state 관련 주의 사항
-	1. state 값 변경 시, 해당 값은 무조건 다음 렌더링 사이클에 반영됨. 
-	2. (1)의 특성에 기인해서, 특정 숫자값을 증감시킬 때에는 후위증감연산자를 사용하면 안 됨. 
-*/
+import Modal from './modal/Modal';
 
 export default function App() {
-	console.log('render');
-	const [Index, setIndex] = useState(0);
-
 	return (
 		<>
-			<button onClick={() => setIndex(Index - 1)}>left</button>
-			<button onClick={() => setIndex(Index + 1)}>right</button>
-			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
+			<h1>Parent</h1>
+			<button>open</button>
+
+			<Modal />
 		</>
 	);
 }
---
+++ src/modal/Modal.jsx
@@ -0,0 +1,10 @@
+import './Modal.scss';
+
+export default function Modal() {
+	return (
+		<aside className='Modal'>
+			<h1>Modal</h1>
+			<button>close</button>
+		</aside>
+	);
+}
